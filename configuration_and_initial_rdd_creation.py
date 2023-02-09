09270876425dcefdbee13eaefc113b950832bde1
phuquanquan <tranphuquan0101@gmail.com>
2023-02-09 23:40:48 +0700

    Add 1(a, b)

---
+++ configuration_and_initial_rdd_creation.py
@@ -0,0 +1,34 @@
+import sys
+import os
+from test_helper import Test
+
+baseDir = os.path.join('databricks-datasets')
+inputPath = os.path.join('cs100', 'lab2', 'data-001', 'apache.access.log.PROJECT')
+logFile = os.path.join(baseDir, inputPath)
+
+def parseLogs():
+    """ Read and parse log file """
+    parsed_logs = (sc
+                   .textFile(logFile)
+                   .map(parseApacheLogLine)
+                   .cache())
+
+    access_logs = (parsed_logs
+                   .filter(lambda s: s[1] == 1)
+                   .map(lambda s: s[0])
+                   .cache())
+
+    failed_logs = (parsed_logs
+                   .filter(lambda s: s[1] == 0)
+                   .map(lambda s: s[0]))
+    failed_logs_count = failed_logs.count()
+    if failed_logs_count > 0:
+        print 'Number of invalid logline: %d' % failed_logs.count()
+        for line in failed_logs.take(20):
+            print 'Invalid logline: %s' % line
+
+    print 'Read %d lines, successfully parsed %d lines, failed to parse %d lines' % (parsed_logs.count(), access_logs.count(), failed_logs.count())
+    return parsed_logs, access_logs, failed_logs
+
+
+parsed_logs, access_logs, failed_logs = parseLogs()
