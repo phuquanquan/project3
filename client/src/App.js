c7ba5a4bd7ee7def70869fb08faecb65101b0c35
phuquanquan <tranphuquan0101@gmail.com>
2023-02-28 20:49:20 +0700

    Add folder components in folder client

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
+import Dashboard from './components/Dashboard';
+
+// function App() {
+//   const [message, setMessage] = useState('');
+
+//   useEffect(() => {
+//     axios.get('http://localhost:5000/api')
+//       .then(response => setMessage(response.data.message))
+//       .catch(error => console.log(error));
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>{message}</h1>
+//     </div>
+//   );
+// }
 
 function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    axios.get('http://localhost:5000/api')
-      .then(response => setMessage(response.data.message))
-      .catch(error => console.log(error));
-  }, []);
-
   return (
-    <div>
-      <h1>{message}</h1>
+    <div className="App">
+      <Dashboard />
     </div>
   );
 }
